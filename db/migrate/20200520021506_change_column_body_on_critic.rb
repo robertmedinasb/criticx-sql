1af23bdeb231ac7594854a71d01627372ca70837
robertmedinasb <robertmedib1@gmail.co>
2020-05-19 21:37:45 -0500

    Undo and redo our last changes

--- db/migrate/20200520021506_change_column_body_on_critic.rb
+++ db/migrate/20200520021506_change_column_body_on_critic.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class ChangeColumnBodyOnCritic < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_column :critics, :body, :text
+  end
+
+  def down
+    change_column :critics, :body, :string
   end
 end
