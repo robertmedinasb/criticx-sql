6a1f6270997d102cc2d895508e2e0971400ef95c
robertmedinasb <robertmedib1@gmail.co>
2020-05-19 22:19:16 -0500

    Adding validations to the Game table

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,7 @@
 class Game < ApplicationRecord
   has_many :critic
   belong_to :company
+  validates :name, :genre, :price, :release_date, :company_id, presence: true
+  validates :genre, inclusion: { in: %w(Simulator Adventure Strategy Role-playing (RPG) Shooter Fighting Sport) }
+  validates :price, numericality: { greather_than: 0 }
 end
