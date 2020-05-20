5e19fd32d7463f4ab5f467567d4ba55735060cc4
robertmedinasb <robertmedib1@gmail.co>
2020-05-19 20:44:59 -0500

    Updating Critic and Game tables

---
+++ db/migrate/20200520014241_add_column_title_to_critic.rb
@@ -0,0 +1,5 @@
+class AddColumnTitleToCritic < ActiveRecord::Migration[6.0]
+  def change
+    add_column :critics, :title, :string
+  end
+end
---
+++ db/migrate/20200520014400_add_references_to_game.rb
@@ -0,0 +1,5 @@
+class AddReferencesToGame < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :games, :company, null: false, foreign_key: true
+  end
+end
