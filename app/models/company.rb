9385e09f811723e8c093b5e5f7ca0e9b42e79dce
robertmedinasb <robertmedib1@gmail.co>
2020-05-19 22:10:31 -0500

    Add validations to the Company Table

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Company < ApplicationRecord
+  has_many :game
+  validates :nane, :country, presence: true
+  validates :name, uniqueness: true, length: { maximum: 40 }
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Game < ApplicationRecord
+  has_many :critic
+  belong_to :company
 end
